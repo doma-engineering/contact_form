1da9f84e53bad5c9a528c70898459d1d9d505716
Jonn <jm@memorici.de>
2021-07-31 02:38:17 +0100

    Problem: 3s invalidating by Janitor drops bans
    Solution:
    
     - Use `expire_at` to bump TTLs outside transaction
    ( see https://github.com/whitfin/cachex/issues/277 )

--- lib/contact_form_web/controllers/contact_controller.ex
+++ lib/contact_form_web/controllers/contact_controller.ex
@@ -6,6 +6,11 @@ defmodule ContactFormWeb.ContactController do
   use ContactFormWeb, :controller
   require Logger
 
+  defp handle(conn), do: conn |> json("ok")
+
+  defp reject(conn, {_, next_allowed}),
+    do: conn |> put_status(429) |> json(%{"allowedFrom" => next_allowed})
+
   @spec index(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def index(conn, _params) do
     ip_maybe = Map.get(conn.assigns, :ip)
@@ -19,49 +24,44 @@ defmodule ContactFormWeb.ContactController do
     ip = ip_maybe
     tau0 = DateTime.utc_now()
 
-    {:ok, {accept, front}} =
-      Cachex.transaction(:nimrod, [ip], fn sentinel ->
-        # Logger.warn(
-        #   "locking #{inspect(ip)} @ #{inspect(DateTime.utc_now())} @ #{
-        #     inspect(:crypto.hash(:blake2b, inspect(conn, limit: 10000)))
-        #     |> Base.encode64()
-        #     |> String.slice(0..7)
-        #   }"
-        # )
+    Logger.info(
+      "Anti-spam for #{ip |> String.slice(0..7)} expires at #{inspect(Cachex.ttl(:nimrod, ip))}"
+    )
 
+    {:ok, %{accept: accept, front: front, expire_at: expire_at}} =
+      Cachex.transaction(:nimrod, [ip], fn sentinel ->
         {:ok, front} = Cachex.get(sentinel, ip)
 
         accept =
           case front do
             nil ->
-              Cachex.put(sentinel, ip, next_second())
+              Cachex.put(sentinel, ip, next_second(), ttl: 2000)
               true
 
             {_, next_allowed} ->
               tau0 >= next_allowed
           end
 
-        if accept do
-          Cachex.update(sentinel, ip, next_second())
-        else
-          Logger.warn("Spam from #{ip |> String.slice(0..7)} @ #{inspect(front)}")
-          Cachex.update(sentinel, ip, exponential_rate_limiter(front))
-        end
+        front1 =
+          {_, tau1} =
+          if accept do
+            front1 = next_second()
+            Cachex.update(sentinel, ip, front1)
+            front1
+          else
+            Logger.warn("Spam from #{ip |> String.slice(0..7)} @ #{inspect(front)}")
+            front1 = exponential_rate_limiter(front)
+            Cachex.update(sentinel, ip, front1)
+            front1
+          end
 
-        # Logger.warn(
-        #   "unlocking #{inspect(ip)} @ #{inspect(DateTime.utc_now())} @ #{
-        #     inspect(:crypto.hash(:blake2b, inspect(conn, limit: 10000)))
-        #     |> Base.encode64()
-        #     |> String.slice(0..7)
-        #   }"
-        # )
-
-        {accept, front}
+        %{accept: accept, front: front1, expire_at: 1000 * (tau1 |> DateTime.to_unix()) + 1000}
       end)
 
     if accept do
       handle(conn)
     else
+      Cachex.expire_at(:nimrod, ip, expire_at)
       reject(conn, front)
     end
   end
@@ -78,9 +78,4 @@ defmodule ContactFormWeb.ContactController do
   defp exponential_rate_limiter(x), do: x
 
   defp next_second(), do: {1, DateTime.utc_now() |> DateTime.add(1)}
-
-  defp handle(conn), do: conn |> json("ok")
-
-  defp reject(conn, {_, next_allowed}),
-    do: conn |> put_status(429) |> json(%{"allowedFrom" => next_allowed})
 end
