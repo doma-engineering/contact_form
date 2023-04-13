22e218d1915fae3e91c5542d91ce79d398bbbea9
Alex Maslo <maslo.rails@gmail.com>
2023-04-13 17:33:54 +0300

    fix: remove grabage comments from sensetive.example.exs file

--- config/sensetive.example.exs
+++ config/sensetive.example.exs
@@ -10,15 +10,15 @@ config :contact_form, ContactForm.Mailer,
   port: 1025,
   username: {:system, "SMTP_USERNAME"},
   password: {:system, "SMTP_PASSWORD"},
-  tls: :if_available, # can be `:always` or `:never`
-  allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"], # or {:system, "ALLOWED_TLS_VERSIONS"} w/ comma separated values (e.g. "tlsv1.1,tlsv1.2")
+  tls: :if_available,
+  allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"],
   tls_log_level: :error,
-  tls_verify: :verify_peer, # optional, can be `:verify_peer` or `:verify_none`
-  tls_cacertfile: "/somewhere/on/disk", # optional, path to the ca truststore
-  tls_cacerts: "…", # optional, DER-encoded trusted certificates
-  tls_depth: 3, # optional, tls certificate chain depth
-  tls_verify_fun: {&:ssl_verify_hostname.verify_fun/3, check_hostname: "example.com"}, # optional, tls verification function
-  ssl: false, # can be `true`
+  tls_verify: :verify_peer,
+  tls_cacertfile: "/somewhere/on/disk",
+  tls_cacerts: "…",
+  tls_depth: 3,
+  tls_verify_fun: {&:ssl_verify_hostname.verify_fun/3, check_hostname: "example.com"},
+  ssl: false,
   retries: 1,
-  no_mx_lookups: false, # can be `true`
-  auth: :if_available # can be `:always`. If your smtp relay requires authentication set it to `:always`.
+  no_mx_lookups: false,
+  auth: :if_available
